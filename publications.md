5378c7237686616bb0d48ca4a288813e97df88df
Guangchuang Yu <guangchuangyu@gmail.com>
2021-10-11 17:42:05 +0800

    chapter 1

---
+++ publications.md
@@ -0,0 +1,17 @@
+\newpage
+
+# Publications of the `r Biocpkg("ggtree")` package suite
+
+
+1. S Xu, Z Dai, P Guo, X Fu, S Liu, L Zhou, W Tang, T Feng, M Chen, L Zhan, T Wu, E Hu, Y Jiang<sup>\*</sup>, X Bo<sup>\*</sup>, __G Yu__<sup>\*</sup>. ggtreeExtra: Compact visualization of richly annotated phylogenetic data. __*Molecular Biology and Evolution*__. 2021, 38(9):4039-4042.
+doi: [10.1093/molbev/msab166](https://doi.org/10.1093/molbev/msab166)
+2. __G Yu__. Using ggtree to visualize data on tree-like structures. __*Current Protocols in Bioinformatics*__, 2020, 69:e96. doi: [10.1002/cpbi.96](https://doi.org/10.1002/cpbi.96)
+3. LG Wang, TTY Lam, S Xu, Z Dai, L Zhou, T Feng, P Guo, CW Dunn, BR Jones, T Bradley, H Zhu, Y Guan, Y Jiang, __G Yu__^\*^. treeio: an R package for phylogenetic tree input and output with richly annotated and associated data. __*Molecular Biology and Evolution*__. 2020, 37(2):599-603.
+doi: [10.1093/molbev/msz240](http://dx.doi.org/10.1093/molbev/msz240)
+4. __G Yu__^\*^, TTY Lam, H Zhu, Y Guan^\*^. Two methods for mapping and visualizing associated data on phylogeny using ggtree. __*Molecular Biology and Evolution*__. 2018, 35(2):3041-3043.
+doi: [10.1093/molbev/msy194](https://doi.org/10.1093/molbev/msy194)
+5. __G Yu__, DK Smith, H Zhu, Y Guan, TTY Lam^\*^. ggtree: an R package for
+visualization and annotation of phylogenetic trees with their covariates and
+other associated data. __*Methods in Ecology and Evolution*__. 2017, 8(1):28-36.
+doi: [10.1111/2041-210X.12628](https://doi.org/10.1111/2041-210X.12628)
+
---
+++ setup.R
@@ -0,0 +1,105 @@
+library(conflicted)
+library(ape)
+library(Biostrings)
+## library(OutbreakTools)
+library(igraph)
+library(phylobase)
+
+library(emojifont)
+library(ggplot2)
+library(dplyr)
+library(kableExtra)
+library(tidytree)
+library(treeio)
+library(ggtree)
+
+library(cowplot)
+library(patchwork)
+library(aplot)
+
+conflict_prefer("expand", "ggtree")
+conflict_prefer("mask", "treeio")
+conflict_prefer("parent", "treeio")
+conflict_prefer("ancestor", "treeio")
+conflict_prefer("get.data", "treeio")
+conflict_prefer("drop.tip", "treeio")
+conflict_prefer("read.newick", "treeio")
+conflict_prefer("MRCA", "tidytree")
+
+conflict_prefer("filter", "dplyr")
+conflict_prefer("rename", "dplyr")
+conflict_prefer("collapse", "dplyr")
+conflict_prefer("intersect", "dplyr")
+conflict_prefer("union", "dplyr")
+conflict_prefer("slice", "dplyr")
+conflict_prefer("strsplit", "base")
+conflict_prefer("paste", "base")
+conflict_prefer("geom_errorbarh", "ggplot2")
+conflict_prefer("as.data.frame", "BiocGenerics")
+
+
+
+
+theme_set(theme_grey())
+
+CRANpkg <- function (pkg) {
+    cran <- "https://CRAN.R-project.org/package"
+    fmt <- "[**%s**](%s=%s)"
+    sprintf(fmt, pkg, cran, pkg)
+}
+
+Biocpkg <- function (pkg) {
+    sprintf("[**%s**](http://bioconductor.org/packages/%s)", pkg, pkg)
+}
+
+Githubpkg <- function (user, pkg) {
+    gh <- "https://github.com"
+    fmt <- "[**%s**](%s/%s/%s)"
+    sprintf(fmt, pkg, gh, user, pkg)
+}
+
+mypkg <- function(pkg, url) {
+    fmt <- "[**%s**](%s)"
+    sprintf(fmt, pkg, url)
+}
+
+pkg_paml <- mypkg("PAML", "http://abacus.gene.ucl.ac.uk/software/paml.html")
+pkg_baseml <- mypkg("BASEML", "http://abacus.gene.ucl.ac.uk/software/paml.html")
+pkg_codeml <- mypkg("CODEML", "http://abacus.gene.ucl.ac.uk/software/paml.html")
+pkg_hyphy <- mypkg("HyPhy", "https://veg.github.io/hyphy-site/")
+pkg_r8s <- mypkg("r8s", "http://loco.biosci.arizona.edu/r8s/")
+pkg_raxml <- mypkg("RAxML", "http://evomics.org/learning/phylogenetics/raxml/")
+pkg_phyldog <- mypkg("PHYLDOG", "http://pbil.univ-lyon1.fr/software/phyldog/") 
+pkg_revbayes <- mypkg("RevBayes", "http://revbayes.github.io/intro.html")
+pkg_archaeopteryx <- mypkg("Archaeopteryx", "https://sites.google.com/site/cmzmasek/home/software/archaeopteryx")
+pkg_phyloch <- mypkg("PHYLOCH", "http://www.christophheibl.de/Rpackages.html")
+pkg_beast <- mypkg("BEAST", "http://beast2.org/")
+pkg_epa <- mypkg("EPA", "http://sco.h-its.org/exelixis/web/software/epa/index.html")
+pkg_pplacer <- mypkg("PPLACER", "http://matsen.fhcrc.org/pplacer/")
+pkg_astral <- mypkg("ASTRAL", "https://github.com/smirarab/ASTRAL")
+pkg_mega <- mypkg("MEGA", "https://www.megasoftware.net/")
+pkg_mrbayes <- mypkg("MrBayes", "http://nbisweden.github.io/MrBayes/")
+
+
+badge_version <- function(pkg, color="green") {
+    v <- packageVersion(pkg)
+    url <- paste0("https://github.com/YuLab-SMU/", pkg)
+    badger::badge_custom(pkg, v, color, url)
+}
+
+
+## based on https://bookdown.org/yihui/rmarkdown-cookbook/fig-process.html
+
+svg2png <- function(path, options) {
+    if (!grepl('[.]svg$', path)) return(path)
+    
+    if (file.size(path)/1000000 < 1.2) {
+        ## less than 1.2M
+        return(path)
+    }
+
+    output <- sub(".svg$", ".png", path)
+    system2("convert", paste("-density 150", path, output))
+    file.remove(path)
+    return(output)
+}
